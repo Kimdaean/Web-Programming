2a7412711678461169d3927c222932fb6a34cd89
DaeHanKim <96115989+Kimdaean@users.noreply.github.com>
2022-01-09 20:20:58 +0900

    Create shuffle.php

---
+++ shuffle.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$arr=range(1,45);
+shuffle($arr);
+echo $arr[0]."  ";
+echo $arr[1]."  ";
+echo $arr[2]."  ";
+echo $arr[3]."  ";
+echo $arr[4]."  ";
+?>
+</body>
+</html>
